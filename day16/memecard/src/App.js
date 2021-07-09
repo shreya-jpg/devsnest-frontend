f315aae80262434f02b259d1f23bbc9840fac182
Shreya Shankar <ishreyashankar@gmail.com>
2021-07-09 17:00:35 +0530

    THA16 created

---
+++ day16/memecard/src/App.js
@@ -0,0 +1,11 @@
+import Card from './components/Card';
+import './App.css';
+
+function App() {
+  return (
+    <Card />
+    
+  );
+}
+
+export default App;
---
+++ day16/memecard/src/components/Card.jsx
@@ -0,0 +1,9 @@
+const Card = () => {
+    return(
+        <div className="Card">
+            <img src= "https://s13.favim.com/orig/161126/air-bang-big-love-Favim.com-4894623.jpeg" alt= "bbt meme" />
+            <h2>React card</h2>
+        </div>
+    );
+}
+export default Card;

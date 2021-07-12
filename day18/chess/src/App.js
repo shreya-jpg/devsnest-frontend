cb3f8305a8efe109cb1e75781bdec6b81654bb9f
Shreya Shankar <ishreyashankar@gmail.com>
2021-07-12 17:12:32 +0530

    updated

--- day18/chess/src/App.js
+++ day18/chess/src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+export default function App() {
+  const checkers = [];
+  for (let i = 0; i < 8; ++i) {
+    checkers.push(<div className={`squre`} />);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="chess-board">
+        <div className="row">{checkers}</div>
+        <div className="row">{checkers}</div>
+        <div className="row">{checkers}</div>
+        <div className="row">{checkers}</div>
+        <div className="row">{checkers}</div>
+        <div className="row">{checkers}</div>
+        <div className="row">{checkers}</div>
+        <div className="row">{checkers}</div>
+      </div>
     </div>
   );
 }
 
-export default App;

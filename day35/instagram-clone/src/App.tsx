6c1b76788dcddcac6fb366cbb1bf2e275961aaa0
Shreya Shankar <65847819+shreya-jpg@users.noreply.github.com>
2021-09-09 21:30:55 +0530

    tha updated

---
+++ day35/instagram-clone/src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css';
+import "../node_modules/bootstrap/dist/css/bootstrap.css";
+import "../node_modules/bootstrap/dist/js/bootstrap";
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      
+    </div>
+  );
+}
+
+export default App;

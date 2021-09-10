0621fec7742fcb768df9d8ee67bee72acb9b2e50
Shreya Shankar <65847819+shreya-jpg@users.noreply.github.com>
2021-09-10 20:40:33 +0530

    tha updated

--- day35/instagram-clone/src/components/Navbar.tsx
+++ day35/instagram-clone/src/components/Navbar.tsx
@@ -3,48 +3,51 @@ import React from 'react'
 const Navbar = () => {
     return(
         <div>
-            <nav className="navbar navbar-expand-lg navbar-light bg-light">
-  <div className="container">
-    <a className="navbar-brand" href="#">
-        <img src="/img/logo.png"></img>
-    </a>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-        <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="#">
-          <i className="bi bi-house-door-fill"></i>
-          </a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#">
-          <i className="bi bi-chat"></i>
-          </a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#">
-          <i className="bi bi-compass"></i>
-          </a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#">
-          <i className="bi bi-heart"></i>
-          </a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#">
-          <i className="bi bi-person-circle"></i>
-          </a>
-        </li>
-      </ul>
+            <nav className="navbar navbar-expand-lg card">
+                <div className="container">
+                    <a className="navbar-brand" href="#">
+                        <img src="/img/logo.png"></img>
+                    </a>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="search">
+                    <input type="text" className="form-control" placeholder="Search" />
+                </div>
+            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li className="nav-item">
+                        <a className="nav-link active" aria-current="page" href="#">
+                            <i className="bi bi-house-door-fill"></i>
+                        </a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="#">
+                            <i className="bi bi-chat"></i>
+                        </a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="#">
+                            <i className="bi bi-compass"></i>
+                        </a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="#">
+                            <i className="bi bi-heart"></i>
+                        </a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="#">
+                            <i className="bi bi-person-circle"></i>
+                        </a>
+                    </li>
+                </ul>
       
-    </div>
-  </div>
-</nav>
-
         </div>
+    </div>
+    </nav>
+
+            </div>
     )
 }
 
